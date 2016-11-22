ef59d542d028ca6a29741cc2f119a5cd1396afba
iturcanu <ionuts.turcanu@gmail.com>
2016-11-22 20:51:20 +0200

    the adapted index.php for the final version of route

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@
      * Date: 11/15/2016
      * Time: 1:25 PM
      */
-    //echo 'Requested URL ="' .$_SERVER['QUERY_STRING'] .'"';
+//    echo 'Requested URL ="' .$_SERVER['QUERY_STRING'] .'"';
 
     require '../Core/Router.php';
 
@@ -18,11 +18,14 @@
     $router->add('', ['controller' => 'Home', 'action' => 'index']);
     $router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
     $router->add('posts/new', ['controller' => 'Posts', 'action' => 'new']);
+    $router->add('{controller}/{action}');
+    $router->add('admin/{controller}/{action}');
+    $router->add('{controller}/{id:\d+}/{action}');
 
     // Display the routing table
-    //echo '<pre>';
-    //var_dump($router->getRoutes());
-    //echo '</pre>';
+    echo '<pre>';
+   // echo htmlspecialchars(print_r($router->getRoutes(), TRUE));
+    echo '</pre>';
 
     // Match the requested route and also a check for git
 

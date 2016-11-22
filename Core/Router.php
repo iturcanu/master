dcb945ae9c3fe5b795ce8e4d70a48c2c971c897f
ioturcanu <ion.turcanu@endava.com>
2016-11-22 18:55:27 +0200

    the final version of the router

--- Core/Router.php
+++ Core/Router.php
@@ -11,12 +11,26 @@ class Router
     protected $routes = [];
     protected $params;
 
-    public function add($route, $params)
+    public function add($route, $params = [])
     {
+        // Convert the route to a regular expression: escape forward slashes
+        $route = preg_replace('/\//', '\\/', $route);
+
+        // Convert variables e.g. {controller}
+        $route = preg_replace('/\{([a-z]+)\}/', '(?P<\1>[a-z-]+)', $route);
+
+        // Convert variables with custom regular expressions e.g. {id:\d+}
+        $route = preg_replace('/\{([a-z]+):([^\}]+)\}/','(?P<\1>\2)', $route);
+
+        // Add start and end delimiters, and case insensitive flag
+        $route = '/^'.$route.'$/i';
+
         $this->routes[$route] = $params;
     }
 
     /**
+     * Get all the routes from the routing table
+     *
      * @return array
      */
     public function getRoutes()
@@ -26,37 +40,18 @@ class Router
 
     public function match($url)
     {
-        /*
-        foreach($this->routes as $route => $params)
-        {
-            if($url == $route)
-            {
+        foreach($this->routes as $route => $params){
+            if(preg_match($route, $url, $matches)){
+                foreach ($matches as $key => $match){
+                    if (is_string($key)){
+                        $params[$key] = $match;
+                    }
+                }
+
                 $this->params = $params;
                 return true;
             }
-        }*/
-
-        $reg_exp = "/^(?P<controller>[a-z-]+)\/(?P<action>[a-z-]+)$/";
-
-        if (preg_match($reg_exp, $url, $matches))
-        {
-            // Get named capture group values
-            $params = [];
-
-            foreach ($matches as $key => $match)
-            {
-                if(is_string($key))
-                {
-                    $params[$key] = $match;
-                }
-            }
-
-            $this->params = $params;
-            return true;
         }
-
-
-
         return false;
     }
 

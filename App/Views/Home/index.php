5835606e85b4a3f615644f93a632255132a4f529
ioturcanu <ion.turcanu@endava.com>
2016-11-29 19:14:04 +0200

    template

--- App/Views/Home/index.php
+++ App/Views/Home/index.php
@@ -1,17 +1,16 @@
 <!doctype html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Home</title>
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
-<body>
-    <h1>Hello <?= htmlspecialchars($name) ?></h1>
-    <p>he loves <?= htmlspecialchars($color) ?></p>
-    <p>Car : <?= $params['car'] ?></p>
-    <p>model : <?= $params['model'] ?></p>
-    <p>year : <?= $params['year'] ?></p>
-
-</body>
+    <body>
+        <h1>Hello <?= htmlspecialchars($name) ?></h1>
+        <p>he loves <?= htmlspecialchars($color) ?></p>
+        <p>Car : <?= $params['car'] ?></p>
+        <p>model : <?= $params['model'] ?></p>
+        <p>year : <?= $params['year'] ?></p>
+    </body>
 </html>
--- App/Views/Posts/index.php
+++ App/Views/Posts/index.php
@@ -1,15 +1,39 @@
 <!doctype html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Home</title>
+    <title>Posts</title>
+    <link   href="css/bootstrap.min.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
     <h3>Aici</h3>
-    <pre>
- <?php var_dump($users); ?>
-    </pre>
+
+    <table class="table table-striped table-bordered">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email Address</th>
+            <th>Mobile Number</th>
+            <th>Action</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+            foreach ($users as $row) {
+                echo '<tr>';
+                echo '<td>'. $row['first_name'] . '</td>';
+                echo '<td>'. $row['last_name'] . '</td>';
+                echo '<td>'. $row['register_date'] . '</td>';
+                echo '<td>
+                           <a class="btn" href="edit?id='. $row['id'] . '">Read</a>
+                           <a class="btn btn-success" href="edit?id='. $row['id'] . '">Update</a>
+                           <a class="btn btn-danger" href="delete.php?id='. $row['id'] . '">Delete</a>
+                    </td>';
+                echo '</tr>';
+            }
+
+        ?>
 </body>
 </html>

8cfcf6b7bb8fec43013246c57c9e827b4f96193c
ioturcanu <ion.turcanu@endava.com>
2016-12-16 11:51:11 +0200

    work pc changes

--- App/Views/left_sidebar.php
+++ App/Views/left_sidebar.php
@@ -61,12 +61,6 @@
                     <li><a href="Tasks/addNew"><i class="fa fa-edit"></i> Create new</a></li>
                 </ul>
             </li>
-            
-            <li><a href="documentation/index.html"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
-            <li class="header">LABELS</li>
-            <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
-            <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
         </ul>
     </section>
     <!-- /.sidebar -->
